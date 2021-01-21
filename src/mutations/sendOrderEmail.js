853eb64c446b1b8d734f4ad6880926b75fd74860
Robin Tang <robin@qsstudio.co>
2021-01-21 17:43:10 +1100

    Update method for sending order email - Try email template.

--- src/mutations/sendOrderEmail.js
+++ src/mutations/sendOrderEmail.js
@@ -34,6 +34,16 @@ export default async function sendOrderEmail(context, input) {
 
   const { action, dataForEmail, fromShop, language, to } = input;
 
+  const getImageUrl = (metafields) => {
+    const key = "productImage";
+    const s3Url = "https://askbella-product-images.s3-ap-southeast-2.amazonaws.com/" // env file?
+    let value;
+    metafields.forEach((m) => {
+      if (m.key === key) value = m.value;
+    });
+    return s3Url + value;
+  };
+
   // Compile email
   let templateName;
 
@@ -46,12 +56,36 @@ export default async function sendOrderEmail(context, input) {
   } else {
     templateName = `orders/${dataForEmail.order.workflow.status}`;
   }
+  const { order } = dataForEmail;
+  const { shipping, payments, discounts } = order
+  let emailData = {
+      "full_name": shipping[0].address.fullName, // shipping[0].address.fullName
+      "shipping_address": shipping[0].address.address1, //shipping[0].address.address1
+      "order_number": order.referenceId, // order.referenceId
+      "order_date": order.updatedAt, // order.createdAt OR updatedAt (will need to change the format)
+      "contact_number": shipping[0].address.phone, // shipping[0].address.phone
+      "billing_address": payments[0].address.address1, //payments[0].address.address1
+      "shipping_method": shipping[0].shipmentMethod.label, // shipping[0].shipmentMethod.label
+      "payment_method": payments[0].displayName, // payment[0].displayName (will need to change the format perhaps)
+      "product": shipping[0].items.map(item=> {
+        return {
+          "product_image": getImageUrl(item.metafields),
+          "product_description": item.title,
+          "product_price": "$" + item.price.amount.toFixed(2) // (Perhaps, we can add displayPrice to the orders mutation or it should already be there in the new one instead of amount) 
+        }}),
+      "shipping_handling_cost": "$" + shipping[0].invoice.shipping.toFixed(2), // "$" + shipping[0].invoice.shipping.toFixed(2)
+      "sub_total_cost": "$" + shipping[0].invoice.subtotal.toFixed(2), // "$" + shipping[0].invoice.subtotal.toFixed(2)
+      "promo_discount": "$" + discounts[0].amount.toFixed(2), // We just have discount amount not the title 
+      "total_cost": "$" + shipping[0].invoice.total.toFixed(2) // "$" + shipping[0].invoice.total.toFixed(2)
+    }
 
-  await context.mutations.sendEmail(context, {
-    data: dataForEmail,
-    fromShop,
-    templateName,
-    language,
-    to
-  });
+  if (templateName === 'orders/shipped' || templateName === 'orders/new') {
+    await context.mutations.sendEmail(context, {
+      data: emailData,
+      fromShop,
+      templateName,
+      language,
+      to
+    });
+  }
 }
